9302d229440fdca2caaf8de7fcadefe4ca55d20f
MichaelP35 <michael.poplawski35@gmail.com>
2024-02-02 13:00:03 -0600

    bot can now say hello and give inspirational quotes

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import os
+import requests
+import json
 import discord
 from dotenv import load_dotenv
 
@@ -10,15 +12,33 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 
 # Enable specific intents as needed:
-intents.messages = True  # Listen to messages
-intents.guilds = True    # Know about guild (server) updates
+intents.message_content = True  # Listen to messages
 
 # Initialize the client with the specified intents
 client = discord.Client(intents=intents)
 
+# Get a inspirational quote from zenquotes
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return(quote)
+
 # Run the bot
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    
+    if message.content.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
 
 client.run(TOKEN)
