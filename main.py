8144b1b5af73bfbba96796017ee25af80e4a5480
MichaelP35 <michael.poplawski35@gmail.com>
2024-02-02 13:31:46 -0600

    cleaning and formatting

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 import os
-import requests
-import json
+import random
 import discord
+import features.inspire
 from dotenv import load_dotenv
+
 
 # Retrieve token of bot without exposing token
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+
 # Create intents object with default settings
 intents = discord.Intents.default()
+
 
 # Enable specific intents as needed:
 intents.message_content = True  # Listen to messages
 
+
 # Initialize the client with the specified intents
 client = discord.Client(intents=intents)
 
-# Get a inspirational quote from zenquotes
-def get_quote():
-    response = requests.get("https://zenquotes.io/api/random")
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + " -" + json_data[0]['a']
-    return(quote)
 
 # Run the bot
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+
 
 @client.event
 async def on_message(message):
@@ -38,7 +37,8 @@ async def on_message(message):
         await message.channel.send('Hello!')
     
     if message.content.startswith('$inspire'):
-        quote = get_quote()
+        quote = features.inspire.get_quote()
         await message.channel.send(quote)
+
 
 client.run(TOKEN)
